d683f257c2c8983e4a4342e5adbc08bb017852ac
Olivia Marolf <livmarolf@gmail.com>
2021-07-26 16:04:07 -0600

    Further study

--- calc.js
+++ calc.js
@@ -29,6 +29,15 @@ if (mathSymbol === '/'){
 if (mathSymbol === '*'){
 	console.log(num1*num2);
 }
+if (mathSymbol === '**'){
+	console.log(num1*num1);
+}
+if (mathSymbol === "^3"){
+	console.log(num1*num1*num1);
+}
+if (mathSymbol === "%"){
+	console.log(num1%num2);
+}
 	// This line closes the connection to the command line interface.
 	reader.close()
 
